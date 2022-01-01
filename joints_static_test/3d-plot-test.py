048b103225e0f94670dcf00d382fc9c394a8df84
jackypark9852 <jackypark9852@gmail.com>
2022-01-01 21:18:28 +0900

    3d scatter plot static test added

---
+++ joints_static_test/3d-plot-test.py
@@ -0,0 +1,56 @@
+import joints
+import mediapipe as mp
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread("joints_static_test_hand.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+#mp utilities
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+#Detect hand
+hand = mp_hands.Hands(
+    static_image_mode= True,
+    max_num_hands= 2,
+    min_detection_confidence= 0.7)
+
+results = hand.process(image)
+image_joint = joints.joints()
+coords = image_joint.calcCoordinates(image, results)
+
+#plot the landmarks in 3d
+fig = plt.figure(figsize=(4,4))
+ax = fig.add_subplot(111, projection='3d')
+
+for item in coords:
+    x = item[0]
+    y = item[1]
+    z = item[2]
+    ax.scatter(x,y,z) # plot the point (2,3,4) on the figure
+
+plt.show()
+
+#display result
+image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+processed_image = image.copy()
+for landmarks in results.multi_hand_landmarks:
+    mp_drawing.draw_landmarks(processed_image, landmarks, mp_hands.HAND_CONNECTIONS)
+
+# for item in coords:
+#     cv2.drawMarker(processed_image, (item[0], item[1]), (0, 0, 255), markerType=cv2.MARKER_CROSS,
+#                    markerSize=40, thickness=2, line_type=cv2.LINE_AA)
+# cv2.drawMarker(processed_image, (362, 299), (255, 0, 255), markerType=cv2.MARKER_CROSS,
+#                markerSize=40, thickness=2, line_type=cv2.LINE_AA)
+
+processed_image = image_joint.drawAngles(processed_image, results)
+cv2.imshow('test', processed_image)
+# print(image_joint.angleBetween([0,1,0], [1, 0, 0]))
+# cv2.imshow('original', image)
+while True:
+    if cv2.waitKey(10) & 0xFF is ord('q'):
+        cv2.destroyAllWindows()
+        exit(11)
+
+exit(1)

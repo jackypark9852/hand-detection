69b983fc47a2b0b3ca02b9894375f3041e6b105e
jackypark9852 <jackypark9852@gmail.com>
2022-01-06 20:47:23 +0900

    <joint> module: add palm_normal calculation

--- joints.py
+++ joints.py
@@ -2,29 +2,58 @@ import numpy as np
 import mediapipe as mp
 import cv2
 import math
+from enum import IntEnum, auto
+
+class ConnLabel(IntEnum):
+    def _generate_next_value_(self, _start, count, _last_values):
+        """Generate consecutive automatic numbers starting from zero."""
+        return count
+    THUMB_CB = auto()
+    THUMB_MCB = auto()
+    THUMB_PPB = auto()
+    THUMB_DPB = auto()
+    INDEX_FINGER_CB = auto()
+    INDEX_FINGER_MCB = auto()
+    INDEX_FINGER_PPB = auto()
+    INDEX_FINGER_DPB = auto()
+    MIDDLE_FINGER_CB = auto()
+    MIDDLE_FINGER_MCB = auto()
+    MIDDLE_FINGER_PPB = auto()
+    MIDDLE_FINGER_DPB = auto()
+    RING_FINGER_CB = auto()
+    RING_FINGER_MCB = auto()
+    RING_FINGER_PPB = auto()
+    RING_FINGER_DPB = auto()
+    PINKY_FINGER_CB = auto()
+    PINKY_FINGER_MCB = auto()
+    PINKY_FINGER_PPB = auto()
+    PINKY_FINGER_DPB = auto()
+    PALM_NORMAL = auto()
 
 class joints:
     def __init__(self):
         # Constants
         self.LANDMARK_COUNT = 21
+        self.CONNECTION_COUNT = 21
         self.COORD_DIM = 3
+        self.PLACE_HOLDER = 0
 
         # Store connection instructions
-        self.CONNECTIONS_PARENT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 17]
-        self.CONNECTIONS_CHILD =  [0, 1, 2, 3, 0, 5, 6, 7, 0,  9, 10, 11,  0, 13, 14, 15,  0, 17, 18, 19, 13]
-
+        # last for palm_normal
+        self.CONNECTIONS_PARENT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, self.PLACE_HOLDER]
+        self.CONNECTIONS_CHILD =  [0, 1, 2, 3, 0, 5, 6, 7, 0,  9, 10, 11,  0, 13, 14, 15,  0, 17, 18, 19, self.PLACE_HOLDER]
 
         # Store angles to annotate
         # [landmark label, conn1, conn2]
-        self.ANGLES_SHOW   = [0, 0, 0, 1, 1, 1, 0,  0,  0,  0,  0,  0,  0,  0,  0, 1]
-        self.ANGLES_AT     = [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 17]
-        self.ANGLES_PARENT = [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 20]
-        self.ANGLES_CHILD  = [0, 1, 2, 4, 5, 6, 8,  9, 10, 12, 13, 14, 16, 17, 18, 17]
+        self.ANGLES_SHOW_FLAG = [0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0, 1]
+        self.ANGLES_SHOW_AT   = [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, 5]
+        self.ANGLES_CONN1     = [1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19, ConnLabel.PALM_NORMAL]
+        self.ANGLES_CONN2     = [0, 1, 2, 4, 5, 6, 8,  9, 10, 12, 13, 14, 16, 17, 18, 5]
 
         # Coordinates, connections, and angels between connections
         self.landmarks = np.zeros((self.LANDMARK_COUNT, 3))
         self.coord = np.zeros((self.LANDMARK_COUNT, 3))
-        self.conn = np.zeros((self.LANDMARK_COUNT, 3))
+        self.conn = np.zeros((self.CONNECTION_COUNT, 3))
         self.angles = np.zeros((self.LANDMARK_COUNT, 3))
 
 
@@ -40,6 +69,10 @@ class joints:
         coords = self._calc_coordinates(results)
         for idx, (a, b) in enumerate(zip(self.CONNECTIONS_PARENT, self.CONNECTIONS_CHILD)):
             self.conn[idx] = coords[a] - coords[b]
+
+        self.conn[ConnLabel.PALM_NORMAL] = np.cross(
+            self.conn[ConnLabel.PINKY_FINGER_CB], self.conn[ConnLabel.INDEX_FINGER_CB])
+        print(self.conn[ConnLabel.PALM_NORMAL])
         return self.conn
 
     def _angle_between(self, conn1, conn2):
@@ -83,11 +116,12 @@ class joints:
         self.image = image
 
         conn = self._calc_connections(results)
-        for show, idx, a, b in zip(self.ANGLES_SHOW, self.ANGLES_AT, self.ANGLES_PARENT, self.ANGLES_CHILD):
+        for show, idx, a, b in zip(self.ANGLES_SHOW_FLAG, self.ANGLES_SHOW_AT, self.ANGLES_CONN1, self.ANGLES_CONN2):
             if show == 1:
                 landmark = self.coord[idx]
                 conn1 = self.conn[a]
                 conn2 = self.conn[b]
+                print(conn1, conn2)
                 self._label_angle(landmark, conn1, conn2)
 
         return image
--- joints_video_test/3d_joints_video_test.py
+++ joints_video_test/3d_joints_video_test.py
@@ -11,8 +11,8 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 # Image capture settings
 cap = cv2.VideoCapture(0)
-if cap.isOpened():
     # print('CAMERA OPENED')
+if cap.isOpened():
     image_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     image_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     cv2.namedWindow('frame')
@@ -27,10 +27,10 @@ else:
 joints_hand = joints.joints()
 
 # 3d figure setup
-# fig = plt.figure(figsize=(8, 8))
-# ax = fig.add_subplot(111, projection='3d')
-# plt.ion()
-# plt.show()
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+plt.ion()
+plt.show()
 
 # Mediapipe drawing setup
 circle_spec = mp_drawing.DrawingSpec(color=(0, 47, 255), thickness=5, circle_radius=8)
@@ -94,27 +94,29 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
             frame = joints_hand.draw_angles(frame, results)
 
             # calc 3d results
-            # plt.cla()
-            #
-            # x = joints_hand.coord[:,0]
-            # y = joints_hand.coord[:,1]
-            # z = joints_hand.coord[:,2]
-            # ax.scatter(x, 1- y, z)  # plot the point (2,3,4) on the figure
-            # for a, b in zip(joints_hand.CONNECTIONS_PARENT, joints_hand.CONNECTIONS_CHILD):
-            #     arr = np.array([joints_hand.coord[a], joints_hand.coord[b]]).T
-            #     ax.plot(arr[0], 1 - arr[1], arr[2], linewidth=3, color='black')
+            plt.cla()
+
+            x = joints_hand.coord[:,0]
+            y = joints_hand.coord[:,1]
+            z = joints_hand.coord[:,2]
+            ax.scatter(x, 1- y, z)  # plot the point (2,3,4) on the figure
+            for a, b in zip(joints_hand.CONNECTIONS_PARENT, joints_hand.CONNECTIONS_CHILD):
+                arr = np.array([joints_hand.coord[a], joints_hand.coord[b]]).T
+                ax.plot(arr[0], 1 - arr[1], arr[2], linewidth=3, color='black')
+            arr = np.array([joints_hand.coord[5] + joints_hand.conn[20]*3, joints_hand.coord[5]]).T
+            ax.plot(arr[0], 1 - arr[1], arr[2], linewidth=3, color='red')
 
         # display image
         cv2.imshow('frame', frame)
 
         # display 3d scatter plot
-        # ax.set_zlim3d(-0.25, 0.25)  # viewrange for z-axis should be [-4,4]
-        # ax.set_ylim3d(0, 1)  # viewrange for y-axis should be [-2,2]
-        # ax.set_xlim3d(0, 1)  # viewrange for x-axis should be [-2,2]
-        # ax.set_xlabel('x-axis')
-        # ax.set_ylabel('y-axis')
-        # ax.set_zlabel('z-axis')
-        # plt.pause(0.000001)  # Note this correction
+        ax.set_zlim3d(-0.25, 0.25)  # viewrange for z-axis should be [-4,4]
+        ax.set_ylim3d(0, 1)  # viewrange for y-axis should be [-2,2]
+        ax.set_xlim3d(0, 1)  # viewrange for x-axis should be [-2,2]
+        ax.set_xlabel('x-axis')
+        ax.set_ylabel('y-axis')
+        ax.set_zlabel('z-axis')
+        plt.pause(0.000001)  # Note this correction
 
         # termination
         if (cv2.waitKey(10) & 0xFF) == ord('q'):
